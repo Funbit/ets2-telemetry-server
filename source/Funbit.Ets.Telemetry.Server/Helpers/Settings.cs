3ee1b8c9c7bec7d48306fa97f556d1c5d28edfa5
Funbit <contact@funbit.info>
2015-01-11 12:39:23 +0900

    Minor comment tweaks.

--- source/Funbit.Ets.Telemetry.Server/Helpers/Settings.cs
+++ source/Funbit.Ets.Telemetry.Server/Helpers/Settings.cs
@@ -7,18 +7,18 @@ namespace Funbit.Ets.Telemetry.Server.Helpers
 {
     public class Settings
     {
+        #region Settings
+
+        public string DefaultNetworkInterfaceId { get; set; }
+
+        #endregion
+
         const string ApplicationName = "Ets2 Telemetry Server";
         const string SettingsName = "Settings.json";
 
         public static readonly string SettingsDirectory = Path.Combine(
             Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ApplicationName);
-
-        public string DefaultNetworkInterfaceId { get; set; }
-        
-        static string SettingsFileName
-        {
-            get { return Path.Combine(SettingsDirectory, SettingsName); }
-        }
+        static readonly string SettingsFileName = Path.Combine(SettingsDirectory, SettingsName);
 
         public static void Clear()
         {
