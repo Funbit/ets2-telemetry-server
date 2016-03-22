78cb603b9f20a1c87ba12cd63d0ab750a8a642f0
Funbit <contact@funbit.info>
2016-03-23 00:02:17 +0900

    Implemented a keyboard helper class for game interaction.

---
+++ source/Funbit.Ets.Telemetry.Server/Helpers/KeyboardHelper.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+#pragma warning disable 414
+
+namespace Funbit.Ets.Telemetry.Server.Helpers
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
+    [SuppressMessage("ReSharper", "NotAccessedField.Local")]
+    public static class KeyboardHelper
+    {
+        const int INPUT_KEYBOARD = 1;
+        const uint KEYEVENTF_KEYDOWN = 0x0000;
+        const uint KEYEVENTF_KEYUP = 0x0002;
+        const uint KEYEVENTF_SCANCODE = 0x0008;
+
+        struct INPUT
+        {
+            public int type;
+            public InputUnion u;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct InputUnion
+        {
+            [FieldOffset(0)]
+            MOUSEINPUT mi;
+            [FieldOffset(0)]
+            public KEYBDINPUT ki;
+            [FieldOffset(0)]
+            HARDWAREINPUT hi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            int dx;
+            int dy;
+            uint mouseData;
+            uint dwFlags;
+            uint time;
+            IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct KEYBDINPUT
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct HARDWAREINPUT
+        {
+            uint uMsg;
+            ushort wParamL;
+            ushort wParamH;
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetMessageExtraInfo();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        static extern uint MapVirtualKey(uint uCode, uint uMapType);
+
+        const uint MAPVK_VK_TO_VSC = 0x00;
+
+        /// <summary>
+        /// Simulates a key press event for a given key.
+        /// </summary>
+        /// <param name="key">Key to be pressed.</param>
+        public static void PressKey(Keys key)
+        {
+            INPUT[] inputs =
+            {
+                new INPUT
+                {
+                    type = INPUT_KEYBOARD,
+                    u = new InputUnion
+                    {
+                        ki = new KEYBDINPUT
+                        {
+                            wVk = 0,
+                            // scan codes work better with games
+                            wScan = (ushort)MapVirtualKey((uint)key, MAPVK_VK_TO_VSC),
+                            dwFlags = KEYEVENTF_KEYDOWN | KEYEVENTF_SCANCODE,
+                            dwExtraInfo = GetMessageExtraInfo(),
+                        }
+                    }
+                }
+            };
+
+            // send key DOWN
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+            Thread.Sleep(10);
+            inputs[0].u.ki.dwFlags = KEYEVENTF_KEYUP | KEYEVENTF_SCANCODE;
+            // send key UP
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+        }
+    }
+}
